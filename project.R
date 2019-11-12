bbfe58b0f80f81f87a06b7bbcd35d1472f155e89
ana.sapata <ana.ssapata@decsis.pt>
2019-11-12 17:46:57 +0000

    html report

--- project.R
+++ project.R
@@ -5,6 +5,7 @@
 library(caret)
 library(corrplot)
 library(rpart)
+library(rpart.plot)
 library(rattle)
 library(e1071)
 
@@ -36,11 +37,9 @@ training <- training[,-c(5:13)]
 training <- training[,-c(27:32)]
 training <- training[,-c(30:38)]
 training <- training[,-c(43:51)]
-validation <- validation[,-c(5:13)]
-validation <- validation[,-c(27:32)]
-validation <- validation[,-c(30:38)]
-validation <- validation[,-c(43:51)]
 
+dim(training)
+dim(validation)
 # Split the training data between testing and training with 30%, 70% resp.
 set.seed(2711)
 inTrain <- createDataPartition(training$classe, p= 0.7, list = FALSE)
@@ -61,14 +60,42 @@ names(training)[high_cor]
 # 1st - Decision Trees
 set.seed(2711)
 DTFit1 <- rpart(classe ~ ., data=training, method="class")
-fancyRpartPlot(DTFit1)
+#fancyRpartPlot(DTFit1)
+png('DTFit1.png')
+prp(DTFit1)
+dev.off()
 # Predictions
 predictDTFit1 <- predict(DTFit1, testing, type = "class")
 conf_mat_DTFit1 <- confusionMatrix(table(predictDTFit1, testing$classe))
+# Accuracy = .743
 conf_mat_DTFit1
 
 
 # 2nd - Random Forest
-#controlRF <- trainControl(method="cv", number=3, verboseIter=FALSE)
-RFFit1 <- train(classe ~ ., data=training, method="rf")
-modRF1$finalModel
+set.seed(2711)
+controlRF <- trainControl(method="cv", number=3, verboseIter=FALSE)
+RFFit1 <- train(classe ~ ., data=training, method="rf", trControl = controlRF)
+RFFit1$finalModel
+# Predictions
+predictRFFit1 <- predict(RFFit1, testing)
+conf_mat_RFFit1 <- confusionMatrix(table(predictRFFit1, testing$classe))
+# Accuracy = 1
+conf_mat_RFFit1
+plot(RFFit1)
+
+
+# 3rd - Generalized Boosted Regression Models
+set.seed(2711)
+controlGBM <- trainControl(method = "repeatedcv", number = 5, repeats = 1)
+GBMFit1 <- train( classe ~. , data = training, method = "gbm", trControl = controlGBM, verbose = FALSE)
+GBMFit1$finalModel
+print(GBMFit1)
+# Predictions
+predictGBMFit1 <- predict(GBMFit1, testing)
+conf_mat_GBMFit1 <- confusionMatrix(table(predictGBMFit1, testing$classe))
+# Accuracy = .9801
+conf_mat_GBMFit1
+
+
+# The model with the high accuracy was RF, so i will apply it to the validation set
+res <- predict(RFFit1, validation); res
